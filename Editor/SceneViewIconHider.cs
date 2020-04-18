c38a4411c02cb98545fa37ca506f800f2a92e7fc
baba_s <shotababa1206@gmail.com>
2020-04-18 15:01:20 +0900

    fix

--- Editor/SceneViewIconHider.cs
+++ Editor/SceneViewIconHider.cs
@@ -2,25 +2,48 @@
 using System.Reflection;
 using UnityEditor;
 
-namespace KoganeUnityLib
+namespace UniSceneViewIconHider
 {
+	/// <summary>
+	/// Scene ビューのアイコンを非表示にするエディタ拡張
+	/// </summary>
 	public static class SceneViewIconHider
 	{
-		[MenuItem( "Edit/Hide Scene View Icon" )]
-		private static void Hide()
-		{
-			const BindingFlags bindingAttrs =
-				BindingFlags.NonPublic | 
-				BindingFlags.Public | 
-				BindingFlags.Static;
+		//================================================================================
+		// デリゲート（static）
+		//================================================================================
+		public static Action OnHide { private get; set; }
 
+		//================================================================================
+		// 関数（static）
+		//================================================================================
+		/// <summary>
+		/// Unity のメニューから Scene ビューのアイコンを非表示にします
+		/// </summary>
+		[MenuItem( "Edit/UniSceneViewIconHider/Scene ビューのアイコンを非表示" )]
+		private static void HideFromMenu()
+		{
+			if ( OnHide != null )
+			{
+				OnHide();
+				return;
+			}
+
+			Hide();
+		}
+
+		/// <summary>
+		/// Scene ビューのアイコンを非表示にします
+		/// </summary>
+		public static void Hide()
+		{
 			var annotation  = Type.GetType( "UnityEditor.Annotation, UnityEditor" );
 			var classId     = annotation.GetField( "classID" );
 			var scriptClass = annotation.GetField( "scriptClass" );
 
 			var annotationUtility = Type.GetType( "UnityEditor.AnnotationUtility, UnityEditor" );
-			var getAnnotations    = annotationUtility.GetMethod( "GetAnnotations", bindingAttrs );
-			var setIconEnabled    = annotationUtility.GetMethod( "SetIconEnabled", bindingAttrs );
+			var getAnnotations    = annotationUtility.GetMethod( "GetAnnotations", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static );
+			var setIconEnabled    = annotationUtility.GetMethod( "SetIconEnabled", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static );
 
 			var annotations = getAnnotations.Invoke( null, null ) as Array;
 
